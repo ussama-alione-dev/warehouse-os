148edbdb3764fbd546200877421f04fbbfd2a932
alione10-lm <aliouaneoussama89@gmail.com>
2026-07-24 13:20:20 +0100

    Refactor type definitions in next-auth.d.ts for improved readability

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,19 +1,19 @@
-import NextAuth, { DefaultSession } from "next-auth";
+import { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
-  interface Session {
-    user: {
-      id: string;
-    } & DefaultSession["user"];
-  }
+    interface Session {
+        user: {
+            id: string;
+        } & DefaultSession["user"];
+    }
 
-  interface User {
-    id: string;
-  }
+    interface User {
+        id: string;
+    }
 }
 
 declare module "next-auth/jwt" {
-  interface JWT {
-    id: string;
-  }
+    interface JWT {
+        id: string;
+    }
 }
